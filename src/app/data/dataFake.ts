f51687f9241656debd9cd45bcaeb7e65264a9452
Helio Vallim <h.vallim@gmail.com>
2024-02-25 15:53:57 -0300

    feat: cria a content page dinâmica

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,26 @@
+export const dataFake = [
+  {
+    "id": "1",
+    "title": "Saiu a nova versão do Angular",
+    "description": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt voluptates natus sint modi voluptatum autem suscipit ipsa, veritatis ullam esse tempora, tenetur animi eos a nulla dolorum impedit enim est.",
+    "photoCover": "https://vidafullstack.com.br/wp-content/uploads/2023/12/angular.jpg"
+  },
+  {
+    "id": "2",
+    "title": "Cresce o uso de Angular no mercado de frameworks",
+    "description": "ola",
+    "photoCover": "https://logowik.com/content/uploads/images/angular9826.logowik.com.webp"
+  },
+  {
+    "id": "3",
+    "title": "As versões do Angular ao longo do tempo",
+    "description": "ola",
+    "photoCover": "https://res.cloudinary.com/practicaldev/image/fetch/s--9Zu-g3nG--/c_limit%2Cf_auto%2Cfl_progressive%2Cq_auto%2Cw_880/https://2muchcoffee.com/blog/content/images/2020/07/Angular-versions-over-time---Angular-Community-in-a-nutshell.png"
+  },
+  {
+    "id": "4",
+    "title": "Melhore a performance das suas aplicações Angular",
+    "description": "ola",
+    "photoCover": "https://techaffinity.com/blog/wp-content/uploads/2022/01/Tips-to-increase-the-speed-of-Angular-Application.jpg"
+  }
+]
